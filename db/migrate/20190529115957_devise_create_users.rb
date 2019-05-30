8961b1164185365f30d359b6ffabc77276a7d211
smoto-shei <smoto_shei@yahoo.co.jp>
2019-05-30 18:35:10 +0900

    Made devise's view

--- db/migrate/20190529115957_devise_create_users.rb
+++ db/migrate/20190529115957_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :avator
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,10 @@
 ActiveRecord::Schema.define(version: 2019_05_29_115957) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "avator"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
