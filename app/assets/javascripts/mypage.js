daa530410c0799202029c992ccdb6be192b6ed11
smoto-shei <smoto_shei@yahoo.co.jp>
2019-06-03 19:44:15 +0900

    fix Turbolinks error
    ターボリンクスによるエラーを解消した

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,12 +1,14 @@
-$(function(){
-  $('#edit_body').on('click',function(){
-    $('.body_modal').css('display', 'block');
-    $('.overlay').css('display', 'block');    $('html').addClass('modal_open');
-  });
+document.addEventListener('turbolinks:load', function() {
+  $(function(){
+    $('#edit_body').on('click',function(){
+      $('.body_modal').css('display', 'block');
+      $('.overlay').css('display', 'block');    $('html').addClass('modal_open');
+    });
 
-  $('.close_button').on('click',function(){
-    $('.body_modal').css('display', 'none');
-    $('.overlay').css('display', 'none');
-    $('html').removeClass('modal_open');
-  })
+    $('.close_button').on('click',function(){
+      $('.body_modal').css('display', 'none');
+      $('.overlay').css('display', 'none');
+      $('html').removeClass('modal_open');
+    })
+  });
 });
