ac776bbdbb538ec8663ffc6b9095cc8a17f2a540
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-06-04 15:33:10 +0900

    merge

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -1,2 +1,12 @@
 class TrainingRecordsController < ApplicationController
+  def create
+    @training_record = TrainingRecord.new(training_record_params)
+    @training_record.menus.build
+    @training_record.save
+  end
+
+  private
+  def training_record_params
+    params.permit(:date, :part, :exercise, menus_attributes:[:id, :training_record_id, :weight, :rep, :time]).merge(user_id: current_user.id)
+  end
 end
--- app/models/training_record.rb
+++ app/models/training_record.rb
@@ -1,2 +1,4 @@
 class TrainingRecord < ApplicationRecord
+  has_many :menus
+  accepts_nested_attributes_for :menus, allow_destroy: true
 end
