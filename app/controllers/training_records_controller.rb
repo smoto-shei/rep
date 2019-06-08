a9ab9e7756005328d961e422863990da4ff81cbc
smoto-shei <smoto_shei@yahoo.co.jp>
2019-06-08 20:56:53 +0900

    making chart from DB
    Chart をDBからデータを引っ張って描画

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -1,7 +1,11 @@
 class TrainingRecordsController < ApplicationController
-     def index
+    before_action :set_user, except: [:create]
+    before_action :set_userbody,except: [:create]
+    before_action :set_month, except: [:index, :create]
+
+
+    def index
       # @records = User.find(current_user.id).trainig_records
-      @user = User.find(current_user.id)
     end
 
     def create
@@ -20,70 +24,71 @@ class TrainingRecordsController < ApplicationController
     end
 
     def chest
-      @user = User.find(current_user.id)
-      @chest = TrainingRecord.where(user_id: current_user.id)
-      @training_records = @user.training_records
-      @userbody = @user.user_body
-      gon.label = [1,2,3,4,5,6]
-      gon.data = [300,300,300,300,300,300]
+      @training_records = @user.training_records.where(part: '胸')
+      gon.part = '胸'
+      gon.label = (@mon-5..@mon).to_a.map {|a| a.to_s + '月'}
+      gon.data = TrainingRecord.make_chart_data(@training_records)
       render :index
     end
 
     def sholder
-      @user = User.find(current_user.id)
-      @chest = TrainingRecord.where(user_id: current_user.id)
-      @training_records = @user.training_records
-      @userbody = @user.user_body
-      gon.label = [1,2,3,4,5,6]
-      gon.data = [300,200,100,300,300,150]
+      @training_records = @user.training_records.where(part: '肩')
+      gon.part = '肩'
+      gon.label = (@mon-5..@mon).to_a.map {|a| a.to_s + '月'}
+      gon.data = TrainingRecord.make_chart_data(@training_records)
       render :index
     end
 
     def arm
-      @user = User.find(current_user.id)
-      @chest = TrainingRecord.where(user_id: current_user.id)
-      @training_records = @user.training_records
-      @userbody = @user.user_body
-      gon.label = [1,2,3,4,5,6]
-      gon.data = [2100,1300,1900,1500,2000,1000]
+      @training_records = @user.training_records.where(part: '腕')
+      gon.part = '腕'
+      gon.label = (@mon-5..@mon).to_a.map {|a| a.to_s + '月'}
+      gon.data = TrainingRecord.make_chart_data(@training_records)
       render :index
     end
 
     def back
-      @user = User.find(current_user.id)
-      @chest = TrainingRecord.where(user_id: current_user.id)
-      @training_records = @user.training_records
-      @userbody = @user.user_body
-      gon.label = [1,2,3,4,5,6]
-      gon.data = [800,900,500,250,700,500]
+      @training_records = @user.training_records.where(part: '背中')
+      gon.part = '背中'
+      gon.label = (@mon-5..@mon).to_a.map {|a| a.to_s + '月'}
+      gon.data = TrainingRecord.make_chart_data(@training_records)
       render :index
     end
 
     def leg
-      @user = User.find(current_user.id)
-      @chest = TrainingRecord.where(user_id: current_user.id)
-      @training_records = @user.training_records
-      @userbody = @user.user_body
-      gon.label = [1,2,3,4,5,6]
-      gon.data = [700,1200,1000,900,400,800]
+      @training_records = @user.training_records.where(part: '脚')
+      gon.part = '脚'
+      gon.label = (@mon-5..@mon).to_a.map {|a| a.to_s + '月'}
+      gon.data = TrainingRecord.make_chart_data(@training_records)
       render :index
     end
 
     def aerobic
-      @user = User.find(current_user.id)
-      @chest = TrainingRecord.where(user_id: current_user.id)
-      @training_records = @user.training_records
-      @userbody = @user.user_body
-      gon.label = [1,2,3,4,5,6]
-      gon.data = [3000,4000,3200,3550,3900,2900]
+      @training_records = @user.training_records.where(part: '有酸素')
+      gon.part = '有酸素'
+      gon.label = (@mon-5..@mon).to_a.map {|a| a.to_s + '月'}
+      gon.data = TrainingRecord.make_chart_data_aerobic(@training_records)
       render :index
     end
 
 
 
   private
+
   def training_record_params
     params.require(:training_record).permit(:date, :part, :exercise, menus_attributes: [:weight, :rep, :time]).merge(user_id: current_user.id)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def set_userbody
+    @userbody = @user.user_body
+  end
+
+  def set_month
+    @mon = Date.today.month
   end
 
 end
