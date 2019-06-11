442bc161d5bbe08f343da8a9d4005a0896fbac7d
smoto-shei <smoto_shei@yahoo.co.jp>
2019-06-11 20:04:32 +0900

    Make record for images
    イメージテーブルを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,27 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :avator])
   end
 
+  private
+
+  def training_record_params
+    params.require(:training_record).permit(:date, :part, :exercise, menus_attributes: [:weight, :rep, :time]).merge(user_id: current_user.id)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def set_userbody
+    @userbody = @user.user_body
+  end
+
+  def set_month
+    @mon = Date.today.month
+    gon.label = (@mon-5..@mon).to_a.map {|a| a.to_s + '月'}
+  end
+
+  def set_gon_user_id
+    gon.user_id = current_user.id
+  end
+
 end
---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,12 @@
+class ImagesController < ApplicationController
+  before_action :set_user, except: [:create]
+  before_action :set_userbody,except: [:create]
+  before_action :set_month, except: :create
+  before_action :set_gon_user_id, except: :create
+
+  def index
+  end
+
+  private
+
+end
