a6ad1266fa9fdfe61bde24c4ab7991374466dc23
smoto-shei <smoto_shei@yahoo.co.jp>
2019-06-13 12:02:21 +0900

    Fix heroic error
    Unknown validation を解消する

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,5 @@
 class Image < ApplicationRecord
   belongs_to :user
   validates :user_image, presence: true
-  validates :user_image, dependent: :destroy
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :training_records
-  has_many :images
+  has_many :training_records, dependent: :destroy
+  has_many :images, dependent: :destroy
   has_one :user_body
 
 end
