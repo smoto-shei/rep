77cfdc9a021db2561e9cc3e831c610f74d2581e5
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-06-14 16:42:22 +0900

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @training_records = @user.training_records
+    @training_records = @user.training_records.includes(:menus)
     @userbody = @user.user_body
     @training_record =  TrainingRecord.new
     @training_record.menus.build
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   end
 
   def records_update
-    @day_records = TrainingRecord.where(user_id: params[:id], date: params[:date])
+    @day_records = TrainingRecord.where(user_id: params[:id], date: params[:date]).includes(:menus)
+    @user = User.find(params[:id])
     render partial: "shared/training_records", collection: @day_records, as: "day_record"
   end
 end
