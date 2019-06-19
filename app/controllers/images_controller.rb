cbabef9754e7e9e09df5c8a5b48313790714d634
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-06-19 12:35:31 +0900

    merge

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,6 +6,28 @@ class ImagesController < ApplicationController
 
   # 筋肉画像のページ
   def index
+    @images = Image.where(user_id: params[:user_id])
+    @image = Image.new
   end
 
+  def create
+    @images = Image.new(image_params)
+    @images.save
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  # 記事投稿のストロングパラメータ
+  def image_params
+    params.require(:image).permit({user_image: []}).merge(user_id: current_user.id)
+  end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ApplicationRecord
   belongs_to :user
   validates :user_image, presence: true
-
+  mount_uploaders :user_image, ImagesUploader
+  serialize :user_image, JSON
 end
