7c9216bc54a6bf0b03c8e8fbf53e9542faf0b396
smoto-shei <smoto_shei@yahoo.co.jp>
2019-06-27 13:06:14 +0900

    Add test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    nickname { 'test-5'}
-    email { 'test@example.com' }
+    nickname { Faker::Pokemon.name }
+    email { Faker::Internet.email }
     password { 123456 }
   end
 end
