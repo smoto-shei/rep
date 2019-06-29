76b51b1f64c94fa210d3d48f1de351dfe2890783
smoto-shei <smoto_shei@yahoo.co.jp>
2019-06-29 18:18:24 +0900

    Config for s3
    S3 の設定

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -5,8 +5,8 @@ class ImagesUploader < CarrierWave::Uploader::Base
   process resize_to_fill: [160,120, 'Center']
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -36,9 +36,9 @@ class ImagesUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
+  # def extension_whitelist
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
