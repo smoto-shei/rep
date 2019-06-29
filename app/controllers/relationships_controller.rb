bb0f79b16decfc255ad0b1425c192de847ca93d8
smoto-shei <smoto_shei@yahoo.co.jp>
2019-06-29 23:54:33 +0900

    Fix error on follow
    フォローができない点を修正した

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,32 +3,32 @@ class RelationshipsController < ApplicationController
 
   # フォロー
   def create
-    following = current_user.follow(user)
+    following = current_user.follow(@user)
     if following.save
       flash[:success] = 'フォローしました'
-      redirect_to user
+      redirect_to @user
     else
       flash.now[:alert] = 'フォローに失敗しました'
-      redirect_to user
+      redirect_to @user
     end
   end
 
   # フォロー解除
   def destroy
-    following = current_user.unfollow(user)
+    following = current_user.unfollow(@user)
     if following.destroy
       flash[:success] = 'フォローを解除しました'
-      redirect_to user
+      redirect_to @user
     else
       flash.now[:alert] = 'フォロー解除に失敗しました'
-      redirect_to user
+      redirect_to @user
     end
   end
 
   private
 
   def set_user
-    user = User.find(params[:relationship][:follow_id])
+    @user = User.find(params[:relationship][:follow_id])
   end
 
 end
